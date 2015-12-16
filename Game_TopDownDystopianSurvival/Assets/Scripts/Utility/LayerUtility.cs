b3611bfdeaa3a6d0e11888aa7e39a6309a2d3268
Maxim Tiourin <mixmaxtwo@gmail.com>
2015-12-16 04:54:57 -0800

    Created the LayerUtility class, which will be used for dealing with layers and layer masking in the future

---
+++ Game_TopDownDystopianSurvival/Assets/Scripts/Utility/LayerUtility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Has useful utility functions for dealing with layers and their masks.
+ * @author - Maxim Tiourin
+ */
+public class LayerUtility : MonoBehaviour {
+    public static int ConstructLayerMask(int[] layers) {
+        int layermask = 0;
+        foreach (int layer in layers) {
+            layermask = 1 << layer;
+        }
+
+        return layermask;
+    }
+}
