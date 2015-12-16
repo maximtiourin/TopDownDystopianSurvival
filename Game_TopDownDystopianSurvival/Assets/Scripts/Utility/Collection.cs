4093e652c437d56693f8b33811c7a74d9d7025cd
Maxim Tiourin <mixmaxtwo@gmail.com>
2015-12-16 08:05:14 -0800

    Added a utility workaround Collection class to deal with Unity's bare bones C# libraries

---
+++ Game_TopDownDystopianSurvival/Assets/Scripts/Utility/Collection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Utility static methods for dealing with collections that aren't available to Unity's C#
+ * @author Maxim Tiourin
+ */
+public class Collection {
+    public static bool Contains<T>(IEnumerable collection, T element) {
+        foreach (T e in collection) {
+            if (e.Equals(element)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
