97b80d13b6cf80b60a734b88dc9f2e163ec9db64
Maxim Tiourin <mixmaxtwo@gmail.com>
2015-12-28 11:50:59 -0800

    Started new FizzikLib that will contain useful library functionality, added the beginnings of the PointScaling static class and the SimplePolygon class

---
+++ Game_TopDownDystopianSurvival/Assets/Scripts/Lib/fizzik/SimplePolygon.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FizzikLib {
+    /**
+     * Simple Polygon class that contains a list of vertices of a simple polygon as Vector2 types, with
+     * the polygon defined in clockwise order. Also contains various static methods for the creation of a
+     * Simple Polygon given various other objects.
+     * @author - Maxim Tiourin
+     */
+    public class SimplePolygon {
+        private List<Vector2> vertices;
+
+        public SimplePolygon() {
+            vertices = new List<Vector2>();
+        }
+
+        public void addVertex(Vector2 v) {
+            vertices.Add(v);
+        }
+
+        public List<Vector2> getVertices() {
+            return vertices;
+        }
+
+        /**
+         * Returns a SimplePolygon containing the vertexes of a BoxCollider2D in world space,
+         * taking into account all aspects of its transform.
+         */
+        public static SimplePolygon boxCollider2DToSimplePolygon(BoxCollider2D collider) {
+            float top = collider.offset.y + (collider.size.y / 2f);
+            float bottom = collider.offset.y - (collider.size.y / 2f);
+            float left = collider.offset.x - (collider.size.x / 2f);
+            float right = collider.offset.x + (collider.size.x / 2f);
+
+            Vector3 topLeft = collider.transform.TransformPoint(new Vector3(left, top, 0f));
+            Vector3 topRight = collider.transform.TransformPoint(new Vector3(right, top, 0f));
+            Vector3 bottomLeft = collider.transform.TransformPoint(new Vector3(left, bottom, 0f));
+            Vector3 bottomRight = collider.transform.TransformPoint(new Vector3(right, bottom, 0f));
+
+            SimplePolygon poly = new SimplePolygon();
+            poly.addVertex(new Vector2(bottomLeft.x, bottomLeft.y));
+            poly.addVertex(new Vector2(topLeft.x, topLeft.y));
+            poly.addVertex(new Vector2(topRight.x, topRight.y));
+            poly.addVertex(new Vector2(bottomRight.x, bottomRight.y));
+
+            return poly;
+        }
+    }
+}
