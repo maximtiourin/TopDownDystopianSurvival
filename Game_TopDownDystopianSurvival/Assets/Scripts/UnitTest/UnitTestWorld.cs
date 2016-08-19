64ae3474e8d8ba96284300f047c1c10a521ad461
Maxim Tiourin <mixmaxtwo@gmail.com>
2016-08-19 09:28:08 -0700

    Added to Tile definition, as well as implementing a solution for UnitTesting

---
+++ Game_TopDownDystopianSurvival/Assets/Scripts/UnitTest/UnitTestWorld.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class UnitTestWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Assert.raiseExceptions = true;
+
+        /* ----------------------------------------------------------------------
+         * Tile
+         * ----------------------------------------------------------------------*/
+
+        //Tile.getTileId
+        ushort testtile = Tile.binaryToUshort("1010101001100110");
+        ushort cmp = Tile.binaryToUshort("10101010");
+        Assert.AreEqual(cmp, Tile.getTileId(testtile));
+
+        //Tile.setTileId
+        testtile = Tile.binaryToUshort("1010101001100110");
+        ushort tileid = (ushort) 1;
+        cmp = Tile.binaryToUshort("0000000101100110");
+        Assert.AreEqual(cmp, Tile.setTileId(testtile, tileid));
+
+        //Tile.getIsWall
+        testtile = Tile.binaryToUshort("1010101001100110");
+        Assert.IsFalse(Tile.getIsWall(testtile));
+        testtile = Tile.binaryToUshort("1010101001100111");
+        Assert.IsTrue(Tile.getIsWall(testtile));
+
+        //Tile.setIsWall
+        testtile = Tile.binaryToUshort("1010101001100110");
+        Assert.IsFalse(Tile.getIsWall(Tile.setIsWall(testtile, false)));
+        Assert.IsTrue(Tile.getIsWall(Tile.setIsWall(testtile, true)));
+
+        /* ----------------------------------------------------------------------
+         * ----------------------------------------------------------------------*/
+    }
+}
--- Game_TopDownDystopianSurvival/Assets/Scripts/World/Tile.cs
+++ Game_TopDownDystopianSurvival/Assets/Scripts/World/Tile.cs
@@ -6,13 +6,13 @@ public static class Tile {
      | Tile bit structure (ushort)
      | 0000     0000    0000    0000
      |                             *    0 - floor, 1 - wall
-     |                            *     
+     |                            *     TODO
      | ****     ****                    8-bit tile-id identifier for tile type (grass, dirt, gravel, etc) 0-255
      */
-    public static readonly ushort maskIsWall = 1;       // (0000 0000 0000 0001)
-    public static readonly byte shiftIsWall = 0;
-    public static readonly ushort maskTileId = 65280;   // (1111 1111 0000 0000) Masks out only the tileid bits from the ushort (still needs to be bitshifted to attain the id value)
-    public static readonly byte shiftTileId = 8;
+    public static readonly ushort       maskIsWall      =   1;
+    public static readonly byte         shiftIsWall     =   0;
+    public static readonly ushort       maskTileId      =   binaryToUshort("1111111100000000");
+    public static readonly byte         shiftTileId     =   8;
 
     public static ushort getTileId(ushort tile) {
         ushort res = (ushort) (tile & maskTileId);
@@ -35,5 +35,20 @@ public static class Tile {
         ushort res = (ushort) (tile & maskIsWall);
         res = (ushort) (res >> shiftIsWall);
         return (res == 1) ? (true) : (false);
+    }
+
+    public static ushort setIsWall(ushort aTile, bool isWall) {
+        ushort tile = aTile;
+        ushort flag = (ushort) ((isWall) ? (1) : (0));
+
+        ushort inverse = (ushort) (~maskIsWall);
+        ushort memory = (ushort) (tile & inverse);
+        ushort shift = (ushort) (flag << shiftIsWall);
+
+        return (ushort) (shift | memory);
+    }
+
+    public static ushort binaryToUshort(string binary) {
+        return System.Convert.ToUInt16(binary, 2);
     }
 }
